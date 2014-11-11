fa96356816b8e03677b4da67aaec07852b6f6c92
Hao <sxhanhao@gmail.com>
2014-11-10 22:46:13 -0500

    db configured

--- dota2/dota2/settings.py
+++ dota2/dota2/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	
 )
 
 MIDDLEWARE_CLASSES = (
@@ -58,8 +59,12 @@ WSGI_APPLICATION = 'dota2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dota2',
+		'USER': 'dota2',
+		'PASSWORD': 'dota2',
+		'HOST':'',
+		'PORT':'', 
     }
 }
 
