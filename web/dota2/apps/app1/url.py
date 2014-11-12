edff5d51c7fb4598bcd06f8c0702f1984028ada9
Hao <sxhanhao@gmail.com>
2014-11-11 21:12:49 -0500

    templates changes

--- web/dota2/apps/app1/url.py
+++ web/dota2/apps/app1/url.py
@@ -5,6 +5,12 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'dota2.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'dashboard',dashboard),
+    url(r'^dashboard/',dashboard,{'template_name':'index.html'}),
+    url(r'^table/',dashboard,{'template_name':'tables.html'}),
+    url(r'^chart/',dashboard,{'template_name':'charts.html'}),
+    url(r'^form/',dashboard,{'template_name':'forms.html'}),
+    url(r'^blank-page/',dashboard,{'template_name':'blank-page.html'}),
+    url(r'^bootstrap-grid/',dashboard,{'template_name':'bootstrap-grid.html'}),
+    url(r'^bootstrap-elements/',dashboard,{'template_name':'bootstrap-elements.html'}),
 )
 
--- web/dota2/apps/app1/views.py
+++ web/dota2/apps/app1/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-def dashboard(request):
-    return render_to_response('index.html',{"app":"app1"})
+def dashboard(request,template_name):
+    return render_to_response(template_name,{"app":"app1"})
